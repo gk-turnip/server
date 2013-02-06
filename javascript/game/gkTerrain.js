24b0f51767a7f8f73ad9b1eeb1e4d1814306b89c
Earth4 <help.earthsaverswa@hotmail.com>
2013-02-05 20:07:45 -0800

    More functions; bugfix

--- javascript/game/gkTerrain.js
+++ javascript/game/gkTerrain.js
@@ -1,28 +1,37 @@
 
 //This is still experimental
 //Begin map data
-var MapIndex=new Array("MapDesert","MapOcean","MapFire","MapGrassland","MapBog")
-var MapDesert=new Array("PapayaWhip","IndianRed","LightSalmon","Wheat","Salmon","PaleGoldenRod","LightSalmon","Moccasin","NavajoWhite","SaddleBrown","Peru","Tan","Wheat","Moccasin","IndianRed","SandyBrown","PeachPuff","Bisque","Brown","BlanchedAlmond","Chocolate","Coral","DarkSalmon");
-var MapOcean=new Array("AliceBlue","Aquamarine","Aqua","Blue","CornflowerBlue","CadetBlue","Cyan","DarkSlateBLue","DarkSeaGreen","LightSeaGreen","MediumSeaGreen","MediumSpringGreen","SeaGreen","Teal");
-var MapFire=new Array("Salmon","Red","Orange","OrangeRed","Tomato","Yellow","DimGrey");
-var MapGrassland=new Array("Yellow","YellowGreen","SpringGreen","MediumSeaGreen","MediumStringGreen","LimeGreen","LightGreen","LawnGreen","Green","GreenYellow","ForestGreen","DarkSeaGreen","DarkGreen","Chartreuse","OliveDrab");
-var MapBog=new Array("DarkGoldenRod","DarkGray","DarkKhaki","DarkOliveGreen","Olive","OliveDrab","Peru","SaddleBrown","Sienna");
+var MapData=new Array("PapayaWhip","IndianRed","LightSalmon","Wheat","Salmon","PaleGoldenRod","LightSalmon","Moccasin","NavajoWhite","SaddleBrown","Peru","Tan","Wheat","Moccasin","IndianRed","SandyBrown","PeachPuff","Bisque","Brown","BlanchedAlmond","Chocolate","Coral","DarkSalmon","NEXT_SECTION","AliceBlue","Aquamarine","Aqua","Blue","CornflowerBlue","CadetBlue","Cyan","DarkSlateBLue","DarkSeaGreen","LightSeaGreen","MediumSeaGreen","MediumSpringGreen","SeaGreen","Teal","NEXT_SECTION","Salmon","Red","Orange","OrangeRed","Tomato","Yellow","DimGrey","NEXT_SECTION","Yellow","YellowGreen","SpringGreen","MediumSeaGreen","MediumSpringGreen","LimeGreen","LightGreen","LawnGreen","Green","GreenYellow","ForestGreen","DarkSeaGreen","DarkGreen","Chartreuse","OliveDrab","NEXT_SECTION","DarkGoldenRod","DarkGray","DarkKhaki","DarkOliveGreen","Olive","OliveDrab","Peru","SaddleBrown","Sienna");
 var Rendered=new Array();
 
 function gkRenderMap (mapId,size) {
 	var pos;
 	var k = 1;
-	var wdth = Math.sqrt(size);
 	var map = MapIndex[mapId];
 	var l = map.length
-	for (var i=1; i<=wdth; i++) {
-		for (var j=1; j<=wdth; j++) {
+	if (mapId=0) {
+		k = Math.floor((Math.random()*22)); 
+	}
+	else if (mapId=1) {
+		k = Math.floor((Math.random()*14)+23); 
+	}
+	else if (mapId=2) {
+		k = Math.floor((Math.random()*7)+37); 
+	}
+	else if (mapId=3) {
+		k = Math.floor((Math.random()*15)+44); 
+	}
+	else if (mapId=4) {
+		k = Math.floor((Math.random()*9)+59); 
+	}
+	for (var i=1; i<=size; i++) {
+		for (var j=1; j<=size; j++) {
 			pos.isoXYZ.z = 0
 			pos.isoXYZ.x = i
 			pos.isoXYZ.y = j
-			k = Math.floor((Math.random()*l)+1);
-			gkIsoCreateSingleDiamond(pos.isoXYZ, map[k]);
-			Rendered[k] = map[k];
+			gkIsoCreateSingleDiamond(pos.isoXYZ, MapData[k]);
+			Rendered[k] = MapData[k];
+			k++;
 		}
 	}
 }
@@ -33,4 +42,21 @@ function gkRestorePixel (xv,yv,size) {
 	pos.isoXYZ.y = yv;
 	pos.isoXYZ.z = 0;
 	gkIsoCreateSingleDiamond(pos.isoXYZ, map[k]);
+}
+
+function gkGetBgPixelColor (xv,yv,size) {
+	var i = xv * size + yv;
+	var out = MapData[i];
+	return out;
+}
+
+function gkLoadSpecialMap (location,size) {
+	for (var j=1; j<=size; j++) {
+		pos.isoXYZ.z = 0
+		pos.isoXYZ.x = i
+		pos.isoXYZ.y = j
+		gkIsoCreateSingleDiamond(pos.isoXYZ, MapData[k]);
+		Rendered[k] = MapData[k];
+		k++;
+	}
 }
